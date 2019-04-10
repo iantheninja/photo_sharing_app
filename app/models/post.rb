a396015233bc4331934fb5dc304d10d809a36209
iantheninja <munene.ian@live.com>
2019-04-10 14:13:14 +0300

    image validation added, posts added to home page, routes resources added, styling added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validate :image_presence
+
+  has_one_attached :image
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   resources :users, only: [:show, :edit, :update]
+  resources :posts, only: [:new, :create]
 
   root to: 'home#index'
 end
